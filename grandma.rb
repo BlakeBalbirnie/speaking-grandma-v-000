3d790b68f357390598076ce13d998bd97e91a3ad
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-08 15:26:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,21 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma
+if speak_to_grandma == "HELLO, GRANDMA!"
+  puts "NO, NOT SINCE 1938!"
+elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN"
+else
+  puts "HUH?! SPEAK UP, SONNY!"
+end
+
+dog = "thirsty"
+if dog == "hungry"
+  puts "Refilling food bowl."
+elsif dog == "thirsty"
+  puts "Refilling water bowl."
+else
+  puts "Reading newspaper."
+end
