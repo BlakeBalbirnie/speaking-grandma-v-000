29c24ebac390735abc53620e509bae711ec05bb1
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-08 15:26:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,12 +20,3 @@ elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
 else
   puts "HUH?! SPEAK UP, SONNY!"
 end
-
-dog = "thirsty"
-if dog == "hungry"
-  puts "Refilling food bowl."
-elsif dog == "thirsty"
-  puts "Refilling water bowl."
-else
-  puts "Reading newspaper."
-end
