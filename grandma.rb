dbab0059b468294d198a2290e23bc5b689529757
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-08 15:32:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,18 +13,12 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-
-if phrase == phrase.upcase
-  if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-
-else 
-  puts "NO, NOT SINCE 1938!"
-
-end
-    
-else 
-
-  puts "HUH?! SPEAK UP, SONNY!"
-
-end
+  if phrase == phrase.upcase
+    if phrase == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU TOO PUMPKIN!"
+    else
+      puts "NO, NOT SINCE 1938!"
+    end
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
